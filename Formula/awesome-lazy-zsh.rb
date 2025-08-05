afb056ba0053facb33f123fa4a5e31c70706e218
Ali M. Jaradat <amjaradat01@gmail.com>
2025-08-05 04:57:06 +0300

    Update to v3.0.4 - minor fixes and improvements

--- Formula/awesome-lazy-zsh.rb
+++ Formula/awesome-lazy-zsh.rb
@@ -1,8 +1,8 @@
 class AwesomeLazyZsh < Formula
   desc "Comprehensive Zsh environment manager with plugins, themes, and profiles"
   homepage "https://github.com/AmJaradat01/awesome-lazy-zsh"
-  url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/v3.0.3.tar.gz"
-  sha256 "8f709265b1696c1b813a77f1142c6e85db85259f0c82311b264bc6c43b88db96"
+  url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/v3.0.4.tar.gz"
+  sha256 "5f54597a377d60ab03a24225b00f0130ff49651feed24a80e39577470dcfd99c"
   license "MIT"
 
   depends_on "node"
