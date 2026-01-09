cd9075f4e591cc53d4e6835299db605c93765fa5
Ali M. Jaradat <amjaradat01@gmail.com>
2026-01-09 11:49:09 +0300

    chore: update formula to v3.1.4

--- Formula/awesome-lazy-zsh.rb
+++ Formula/awesome-lazy-zsh.rb
@@ -1,8 +1,8 @@
 class AwesomeLazyZsh < Formula
   desc "Comprehensive Zsh environment manager with 40+ plugins, themes, and profiles"
   homepage "https://github.com/AmJaradat01/awesome-lazy-zsh"
-  url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/refs/tags/v3.1.3.tar.gz"
-  sha256 "c72e72d62eaf6413d34a384c0e0049679f5d38c96d5cf04f71d78f36da3fe890"
+  url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/refs/tags/v3.1.4.tar.gz"
+  sha256 "b3ad3d82c0320c5d98ebc6cf21d077f370b5d4dd03662dcb976d7413a3b47fbc"
   license "MIT"
   version "3.1.3"
 
