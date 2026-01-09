0390b85981df40a07e6e0e6497b97a90c58ae7ef
Ali M. Jaradat <ali.jaradat@shahid.net>
2026-01-09 11:54:03 +0300

    fix: restrict formula to macOS only
    - Add depends_on :macos
    - Remove Ubuntu from CI test matrix
    - Formula uses brew services which is macOS specific

--- Formula/awesome-lazy-zsh.rb
+++ Formula/awesome-lazy-zsh.rb
@@ -4,10 +4,9 @@ class AwesomeLazyZsh < Formula
   url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/refs/tags/v3.1.4.tar.gz"
   sha256 "b3ad3d82c0320c5d98ebc6cf21d077f370b5d4dd03662dcb976d7413a3b47fbc"
   license "MIT"
-  version "3.1.3"
 
-  depends_on "git"
   depends_on "node"
+  depends_on :macos
 
   def install
     libexec.install Dir["*"]
