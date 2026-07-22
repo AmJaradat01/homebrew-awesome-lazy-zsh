6ae059364633ce375cba0ee23b7527996fdbbc54
Ali M. Jaradat <amjaradat01@gmail.com>
2026-07-22 16:39:43 +0300

    chore: bump formula to v3.2.1 - cloud CLIs, DevOps tools, and language runtimes

--- Formula/awesome-lazy-zsh.rb
+++ Formula/awesome-lazy-zsh.rb
@@ -1,8 +1,8 @@
 class AwesomeLazyZsh < Formula
   desc "Comprehensive Zsh environment manager with 40+ plugins, themes, and profiles"
   homepage "https://github.com/AmJaradat01/awesome-lazy-zsh"
-  url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "032fee1bc9c4061d89f95765565425e0d50d0355a2034c1fbaf45083d3d63ea6"
+  url "https://github.com/AmJaradat01/awesome-lazy-zsh/archive/refs/tags/v3.2.1.tar.gz"
+  sha256 "f37778c68569283d6ed6df2257c30d2b8d01e7a8b8a0ee311f7a00d5b58a2876"
   license "MIT"
 
   depends_on "git"
