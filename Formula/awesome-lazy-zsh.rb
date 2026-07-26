44cdbe658a8524dd9d18a6d9b7b120f1df5458c8
Ali M. Jaradat <AmJaradat01@gmail.com>
2026-07-26 14:44:17 +0300

    ci: harden Homebrew release pipeline (#1)

--- Formula/awesome-lazy-zsh.rb
+++ Formula/awesome-lazy-zsh.rb
@@ -5,20 +5,30 @@ class AwesomeLazyZsh < Formula
   sha256 "fb9102efb70cc4ceee54ade3a2b19fa271b36e0f7b824fb92c5f7955f69b64fd"
   license "MIT"
 
+  depends_on "fzf"
   depends_on "git"
   depends_on "node"
+  depends_on "starship"
+  depends_on "zsh"
 
   def install
     libexec.install Dir["*"]
 
+    cd libexec do
+      system "npm", "install", *std_npm_args, "--ignore-scripts"
+    end
+
     (bin/"awesome-lazy-zsh").write <<~EOS
       #!/bin/bash
-      cd "#{libexec}" && exec ./setup.sh "$@"
+      set -euo pipefail
+      if [[ "${1:-}" == "--version" ]]; then
+        exec "#{formula_opt_bin("node")}/node" -p "require('#{libexec}/package.json').version"
+      fi
+      exec "#{formula_opt_bin("node")}/node" "#{libexec}/src/index.js" "$@"
     EOS
   end
 
   test do
-    assert_path_exists bin/"awesome-lazy-zsh"
-    assert_predicate bin/"awesome-lazy-zsh", :executable?
+    assert_match version.to_s, shell_output("#{bin}/awesome-lazy-zsh --version")
   end
 end
